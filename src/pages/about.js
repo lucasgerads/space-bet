b410f4801114513534ff2678a7b51acd3e1e1658
Leon <theleon0226@yahoo.com>
2020-06-03 15:56:50 -0400

    About page draft 1

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,8 +6,18 @@ export default ({ data }) => (
   <Layout>
     <h1>About "{ data.site.siteMetadata.title }"</h1>
     <p>
-      We're the only site running on your computer dedicated to showing the best
-      photos and videos of pandas eating lots of food.
+      In December 2015, SpaceX landed a rocket back on Earth, and Elon Musk promised to take us all to Mars. Leon is 
+      gullible, and he believed him. Lucas is wiser, and was skeptical. 
+      
+      So, they made a bet: if humans made it to Mars by 11:59 pm, December 31, 2029, Leon would be the victor. Otherwise,
+      all the glory of being correct would go to Lucas. 
+
+      Winner gets a coke (and Mars bar). 
+
+      This was all before Elon Musk outed himself as a crazy person, and before the phallic imagery occured to us.
+
+      Still--a bet's a bet.
+
     </p>
   </Layout>
 )
