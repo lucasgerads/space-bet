c32cceba9703774f017eb64e4d28070f70d9a16c
Leon <theleon0226@yahoo.com>
2020-06-03 16:03:26 -0400

    About page draft 1, html tags fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,18 +7,17 @@ export default ({ data }) => (
     <h1>About "{ data.site.siteMetadata.title }"</h1>
     <p>
       In December 2015, SpaceX landed a rocket back on Earth, and Elon Musk promised to take us all to Mars. Leon is 
-      gullible, and he believed him. Lucas is wiser, and was skeptical. 
+      gullible, and he believed him. Lucas is wiser, and was skeptical. </p>
       
-      So, they made a bet: if humans made it to Mars by 11:59 pm, December 31, 2029, Leon would be the victor. Otherwise,
-      all the glory of being correct would go to Lucas. 
+      <p>So, they made a bet: if humans made it to Mars by 11:59 pm, December 31, 2029, Leon would be the victor. Otherwise,
+      all the glory of being correct would go to Lucas. </p>
 
-      Winner gets a coke (and Mars bar). 
+      <p>Winner gets a coke (and Mars bar). </p>
 
-      This was all before Elon Musk outed himself as a crazy person, and before the phallic imagery occured to us.
+      <p>This was all before Elon Musk outed himself as a crazy person, and before the phallic imagery occured to us.</p>
 
-      Still--a bet's a bet.
+      <p>Still--a bet's a bet.</p>
 
-    </p>
   </Layout>
 )
 
